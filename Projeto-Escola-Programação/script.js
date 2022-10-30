46ba196430f901efd2712d2c084d8f1c04d6ceac
AlanRSilvaJW <alan.sithka@gmail.com>
2022-10-30 11:08:38 -0300

    Construi Intro de Arrays

---
+++ Projeto-Escola-Programação/script.js
@@ -0,0 +1,6 @@
+const cursos = ['Javascript ' , ' Html e CSS ' , ' APIs REST']
+const turmas = ['JS T1 ' , ' JS T2 ' , ' Ht/CSS T1 ' , ' Ht/CSS T2 ' , ' APIs T1 ' , ' APIs T2']
+const estudantes = [' Alan ' , ' Sirlene ' , ' Thaynan ' , ' Kaue ' , ' Eliane ' , ' Odete ']
+console.log(cursos)
+console.log(turmas)
+console.log(estudantes)
