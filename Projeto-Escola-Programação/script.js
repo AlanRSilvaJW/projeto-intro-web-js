580b12c309d178611c9152ef54e689fa194966bb
AlanRSilvaJW <alan.sithka@gmail.com>
2022-11-10 21:34:13 -0300

    inclui obejtos

--- Projeto-Escola-Programação/script.js
+++ Projeto-Escola-Programação/script.js
@@ -1,6 +1,33 @@
-const cursos = ['Javascript ' , ' Html e CSS ' , ' APIs REST']
-const turmas = ['JS T1 ' , ' JS T2 ' , ' Ht/CSS T1 ' , ' Ht/CSS T2 ' , ' APIs T1 ' , ' APIs T2']
-const estudantes = [' Alan ' , ' Sirlene ' , ' Thaynan ' , ' Kaue ' , ' Eliane ' , ' Odete ']
-console.log(cursos)
-console.log(turmas)
-console.log(estudantes)
+const turmas = [
+    {
+        turma: "";
+        curso:"";
+        inicio:"";
+        termino:"";
+        numeroDeAlunos: number;
+        período:"";
+        Concluida: boolean
+}
+];
+
+const cursos = [
+    {
+        curso:"";
+        descrição:"";
+        duração:"";
+        valor: number
+}
+];
+
+const estudantes =[
+    {
+        estudante:"";
+        turma:"";
+        curso:"";
+        valor: number;
+        nparcelas:"";
+        desconto: boolean
+        parcelas: number
+
+}
+];
